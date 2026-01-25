c0f0425ce6025c00676866200bb6821ab5986a2a
Parvesh <arveshchaudhary@gmail.com>
2026-01-25 18:25:40 +0545

    Added addtocart animation whenever user click on addtocart

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,7 +1,7 @@
 import { FaShoppingCart } from "react-icons/fa"
 import type { Product } from "../../types/product"
 import { useNavigate } from "react-router-dom"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { CartContext } from "../../context/CartContext"
 
 interface ProductProps {
@@ -12,44 +12,67 @@ interface ProductProps {
 const ProductCard = ({ product }: ProductProps) => {
     const navigate = useNavigate() // for singleProduct
     const context = useContext(CartContext) // CartContext from context
+    const [showAlert, setShowAlert] = useState(false); //for addToCart alert
 
     if (!context) throw new Error("Must be used within CartProvider");
     const { addToCart } = context
 
+    const handleAddToCart = () => {
+        addToCart(product, 1);
+
+        setShowAlert(true);
+        setTimeout(() => setShowAlert(false), 2000);
+    };
+
     return (
-        <div className="border rounded-lg cursor-pointer border-gray-200 hover:scale-105 hover:shadow-xl transition-transform duration-300 bg-white overflow-hidden">
+        <>
+            {/* ADD TO CART ALERT */}
+            {showAlert && (
+                <div className="fixed top-5 right-5 z-50 animate-slide-in">
+                    <div className="bg-white border border-green-500 shadow-lg rounded-lg px-4 py-3 min-w-65">
+                        <p className="text-green-600 font-semibold">
+                            ✅ Added to cart
+                        </p>
+                        <div className="h-1 bg-green-500 mt-2 rounded animate-progress" />
+                    </div>
+                </div>
+            )}
 
-            {/* Product image */}
-            <div>
-                <img
-                    src={product.image}
-                    alt={product.title}
-                    onClick={() => navigate(`/products/${product.id}`)}  // for singleProduct
-                    className="object-contain h-50 w-50"
-                />
+            <div className="border rounded-lg cursor-pointer border-gray-200 hover:scale-105 hover:shadow-xl transition-transform duration-300 bg-white overflow-hidden">
+
+                {/* Product image */}
+                <div>
+                    <img
+                        src={product.image}
+                        alt={product.title}
+                        onClick={() => navigate(`/products/${product.id}`)}  // for singleProduct
+                        className="object-contain h-50 w-50"
+                    />
+                </div>
+
+                {/* Product Info */}
+                <div className="p-3">
+                    <h1 className="line-clamp-2 font-semibold text-gray-800 text-base mb-2">
+                        {product.title}
+                    </h1>
+                    <p className="text-lg font-bold text-gray-900 mb-3">
+                        ${product.price.toFixed(2)}
+                    </p>
+
+
+                    {/* Add to Cart Button */}
+                    <button
+                        // onClick={() => addToCart(product,1)}
+                        onClick={handleAddToCart}
+                        className="bg-red-500 hover:bg-red-600 px-4 py-2 text-base rounded-md text-white w-full flex items-center justify-center gap-2 font-semibold transition-colors duration-200"
+                    >
+                        <FaShoppingCart className="w-5 h-5" />
+                        Add to Cart
+                    </button>
+                </div>
+
             </div>
-
-            {/* Product Info */}
-            <div className="p-3">
-                <h1 className="line-clamp-2 font-semibold text-gray-800 text-base mb-2">
-                    {product.title}
-                </h1>
-                <p className="text-lg font-bold text-gray-900 mb-3">
-                    ${product.price.toFixed(2)}
-                </p>
-
-
-                {/* Add to Cart Button */}
-                <button
-                    onClick={() => addToCart(product,1)}
-                    className="bg-red-500 hover:bg-red-600 px-4 py-2 text-base rounded-md text-white w-full flex items-center justify-center gap-2 font-semibold transition-colors duration-200"
-                >
-                    <FaShoppingCart className="w-5 h-5" />
-                    Add to Cart
-                </button>
-            </div>
-
-        </div>
+        </>
     )
 }
 
